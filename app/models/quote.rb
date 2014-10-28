b376f34329230a70039a96716a181d85362d45b1
cmarkel5 <cmarkel5@gmail.com>
2014-10-28 09:04:42 -0400

    API & nav work in progress

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,19 +1,23 @@
 class Quote < ActiveRecord::Base
-  def self.get(category = null)
-    if category != null
+  
+  def self.get_all
+    categories = %w[funny management sports life inspire love]
+    categories.each do |category|
+      puts "category is #{category}"
       search_url = "https://api.theysaidso.com/qod.json?category=#{category}"
-    else
-      search_url = "https://api.theysaidso.com/qod.json"
+      puts "url defined"
+      response = HTTParty.get search_url
+      puts "response gathered"
+      Quote.create( tss_id:   response["contents"]["id"],
+                    content:  response["contents"]["quote"],
+                    author:   response["contents"]["author"],
+                    category: response["contents"]["category"]
+                  )
+      puts "quote created"
     end
-    
-    response = HTTParty.get search_url
-
-    id = response["contents"]["id"]
-    quote = response["contents"]["quote"]
-    author = response["contents"]["author"]
-    length = response["contents"]["length"]
-    tags = response["contents"]["tags"]
-    category = response["contents"]["category"]
-    response
   end
 end
+
+
+
+  
